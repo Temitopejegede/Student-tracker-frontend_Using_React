9f3d5dbf4f92ae2528c9ffc8fca8ce999dfa2c08
Temitope Jegede <temiteejegede@gmail.com>
2022-05-07 21:58:30 -0400

    Finished Page

--- src/components/Student.js
+++ src/components/Student.js
@@ -1,9 +1,9 @@
 import * as React from "react";
-import Box from "@mui/material/Box";
-// import { makeStyles } from "@mui/styles";
+import { useState } from "react";
 import TextField from "@mui/material/TextField";
 import Container from "@mui/material/Container";
 import Paper from "@mui/material/Paper";
+import Button from "@mui/material/Button";
 
 // const useStyles = makeStyles((theme) => ({
 //   root: {
@@ -15,18 +15,26 @@ import Paper from "@mui/material/Paper";
 
 export default function Student() {
   const paperStyle = { padding: "50px 20px", width: 600, margin: "20px auto" };
-  //   const classes = useStyles();
+  const [name, setName] = useState("");
+  const [address, setAddress] = useState("");
+  const handleClick = (e) => {
+    e.preventDefault();
+    const student = { name, address, setAddress };
+    console.log(student);
+  };
 
   return (
     <Container>
       <Paper elevation={3} style={paperStyle}>
-        <h1 style={{ color: "green" }}>Add a student</h1>
+        <h1 style={{ color: "blue" }}>Add a student</h1>
         <form noValidate autocomplete="off">
           <TextField
             id="standard-basic"
             label="Student Name"
             variant="outlined"
             fullWidth
+            value={name}
+            onChange={(e) => setName(e.target.value)}
           />
 
           <TextField
@@ -35,7 +43,16 @@ export default function Student() {
             label="Student Address"
             variant="outlined"
             fullWidth
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
           />
+          <Button
+            style={{ marginTop: "15px" }}
+            variant="contained"
+            onClick={handleClick}
+          >
+            Submit
+          </Button>
         </form>
       </Paper>
     </Container>
